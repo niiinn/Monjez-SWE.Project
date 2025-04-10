f0c8d1143a682df4f5a1162b96a5158e9fdd7189
layal99 <162923451+layal99@users.noreply.github.com>
2025-04-10 12:12:00 +0300

    Delete Project/Monjez-website/logOut.php

--- Project/Monjez-website/logOut.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();     // تفرغ كل متغيرات الجلسة
-session_destroy();   // تدمر الجلسة بالكامل
-
-header("Location: logIn.php"); // ترجع المستخدم لصفحة تسجيل الدخول
-exit();
